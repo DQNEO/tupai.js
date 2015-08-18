08ff17c05a94bbaebd5fa1aceed453349c28ddca
DQNEO <dqneoo@gmail.com>
2015-08-18 18:00:29 +0900

    ignore gen dir

--- examples/minimal/gen/templates/minimal/Templates.js
+++
@@ -1,7 +0,0 @@
-Package('minimal')
-.define('Templates', function(cp) {
-var _templates = {"minimal.RootViewController.content":"<div>\n    <div data-ch-name=\"lbl\"></div>\n</div>"};
-  return {
-    get : function (name) {
-      return _templates[name];
-    }  };});

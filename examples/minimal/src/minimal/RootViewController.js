74d98dcac91e42b42aaf1a717c1fdb483058759b
DQNEO <dqneoo@gmail.com>
2015-08-31 00:15:46 +0900

    add minimal example

---
+++ examples/minimal/src/minimal/RootViewController.js
@@ -0,0 +1,25 @@
+Package('minimal')
+.use('tupai.ViewController')
+.use('tupai.ui.View')
+.use('minimal.Templates')
+.define('RootViewController', function(cp) { return cp.ViewController.extend({
+    viewInit: function(options, url) {
+        cp.ViewController.prototype.viewInit.apply(this, arguments);
+        var view = new cp.View({
+            template: cp.Templates.get('tmpl1'),
+            templateParameters: {
+                var1: 'Hello World!'
+            }
+        });
+        this.setContentView(view);
+    },
+    viewDidLoad: function (view) {
+        cp.ViewController.prototype.viewDidLoad.apply(this, arguments);
+    },
+    viewDidUnload: function (view) {
+        cp.ViewController.prototype.viewDidUnload.apply(this, arguments);
+    },
+    /* call from transisManager */
+    transitController: function (controller, url, options, transitOptions) {
+    }
+});});

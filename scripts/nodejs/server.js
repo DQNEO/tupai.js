15126affd60949c6fdd39592b904659891d09853
DQNEO <dqneoo@gmail.com>
2016-03-04 15:42:08 +0900

    Revert "call useProxyRequest after use '/'"
    This reverts commit eec1f9e5eb92d869c524d2d5eae6db3afb2c766b.

--- scripts/nodejs/server.js
+++ scripts/nodejs/server.js
@@ -187,6 +187,7 @@ function configDebugMode(app) {
     var webDir = path.join(__dirname, '..', '..', 'releases', mConfig.web);
     //console.log(mConfig);
 
+    useProxyRequest(app);
     app.use('/__js', express.static(mConfig.sources));
     app.use('/__js', express.static(mConfig.genTemplates));
     app.use('/__js', express.static(mConfig.genConfigs));
@@ -216,7 +217,6 @@ function configDebugMode(app) {
         }
     });
 
-    useProxyRequest(app);
     var tupaiRootDir = path.join(__dirname, '..', '..');
     app.use('/__tupairoot', express.static(tupaiRootDir));
 
